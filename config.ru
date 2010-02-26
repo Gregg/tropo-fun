2fdceebeda41cddb5f70a041779999dec0393f9e
Gregg Pollack <gregg@envylabs.com>
2010-02-26 16:22:13 -0500

    added activesupport

--- config.ru
+++ config.ru
@@ -1,2 +1,6 @@
+require 'rubygems'
+require 'sinatra'
+require 'activesupport'
+require 'vendor/tropo-webapi-ruby/lib/tropo-webapi-ruby.rb'
 require 'hello'
 run Sinatra::Application
--- hello.rb
+++ hello.rb
@@ -1,12 +1,8 @@
-require 'rubygems'
-require 'sinatra'
-require 'activesupport'
-require 'vendor/tropo-webapi-ruby/lib/tropo-webapi-ruby.rb'
-
 post '/helloworld.json' do
   
   tropo = Tropo::Generator.new do
-    say 'Hello World!Hello World!Hello World!'
+    say 'Hello World! Hello World! Hello World!'
   end
   tropo.response
+  
 end
