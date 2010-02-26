705f1541ecddeee58dc218cd8c64aaf0a989be8e
Gregg Pollack <gregg@envylabs.com>
2010-02-26 16:18:56 -0500

    added activesupport

--- hello.rb
+++ hello.rb
@@ -3,7 +3,7 @@ require 'sinatra'
 require 'activesupport'
 require 'vendor/tropo-webapi-ruby/lib/tropo-webapi-ruby.rb'
 
-get '/helloworld.json' do
+post '/helloworld.json' do
   
   tropo = Tropo::Generator.new do
     say 'Hello World!'
