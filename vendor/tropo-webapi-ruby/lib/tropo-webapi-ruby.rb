a8e83bf7395b13a658dc76a9f18848fa0f14cf35
Gregg Pollack <gregg@envylabs.com>
2010-02-26 16:34:48 -0500

    added .gem and removed vendor

--- vendor/tropo-webapi-ruby/lib/tropo-webapi-ruby.rb
+++
@@ -1,6 +0,0 @@
-$: << File.expand_path(File.dirname(__FILE__))
-require 'uri'
-require 'json'
-require 'time'
-require 'tropo-webapi-ruby/tropo-webapi-ruby-helpers'
-require 'tropo-webapi-ruby/tropo-webapi-ruby'
