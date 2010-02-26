59e65d3b9ac59b76bccedf9b2dad5062beb05ceb
Gregg Pollack <gregg@envylabs.com>
2010-02-26 16:19:36 -0500

    added activesupport

--- hello.rb
+++ hello.rb
@@ -6,7 +6,7 @@ require 'vendor/tropo-webapi-ruby/lib/tropo-webapi-ruby.rb'
 post '/helloworld.json' do
   
   tropo = Tropo::Generator.new do
-    say 'Hello World!'
+    say 'Hello World!Hello World!Hello World!'
   end
   tropo.response
 end
